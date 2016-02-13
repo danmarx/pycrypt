d7c1823218c1af6e88293197d67fe2f181627213
Dan Marx <marx.dan@gmail.com>
2016-02-12 20:42:40 -0800

    Initial Checkin of Jon's Program

---
+++ message_wirter.py
@@ -0,0 +1,35 @@
+def code_message(message):
+    code = []
+    for char in message:
+        #abcdefghijklmnopqrstuvwxyz
+        
+        #zyxwvutsrqponmlkjihgfedcba
+        if char == 'a':
+            pass
+        elif char == 'b':
+            pass
+        elif char == 'c':
+            pass
+        elif char == 'd':
+            pass
+        elif char == 'e':
+            pass
+        elif char == 'f':
+            pass
+        elif char == 'g':
+            pass
+        elif char == 'h':
+            pass
+        elif char == 'i':
+            pass
+        elif char == 'j':
+            pass
+        elif char == 'k':
+            pass
+        elif char == 'l':
+            pass
+        elif char == 'm':
+            pass
+        elif char == 'n':
+            pass
+            
